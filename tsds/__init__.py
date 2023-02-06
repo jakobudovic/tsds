5044c37879990482a2fdac510880169c6a8db772
jakobudovic <ju8168@student.uni-lj.si>
2023-02-06 18:27:54 +0100

    Add package init file

--- tsds/__init__.py
+++ tsds/__init__.py
@@ -1,5 +1,31 @@
+from tsds.tsds import TimeSeriesDataSegmentation
+
+__author__ = "J.Udovic"
+__email__ = "jakobudovic2@gmail.com"
 __version__ = "0.0.1"
 
+__doc__ = """
+TimeSeriesDataSegmentation
+================
 
-def helloWorld():
-    print("Hello World")
+Description
+-----------
+TimeSeriesDataSegmentation is a Python package created for analysis of any (clinical) time series data.
+
+Example
+-------
+>>> # Import library
+>>> from tsds import TimeSeriesDataSegmentation
+>>> # Initialize with 2 datasets
+>>> tds = TimeSeriesDataSegmentation(df_clustering, df_prediction)
+>>> # Reduce data dimensionality using NMF
+>>> df_clustering_nmf = tds.get_nmf_data(
+    ids_col=ids_col,
+    timestamp_col=timestamp_col,
+)
+
+References
+----------
+* https://github.com/jakobudovic/tsds
+
+"""
