f973fce65020d664c75db23c2b1c73cf5ce78449
jakobudovic <ju8168@student.uni-lj.si>
2023-02-07 11:43:30 +0100

    Return sklearn import

--- tsds/predict.py
+++ tsds/predict.py
@@ -1,5 +1,6 @@
 import numpy as np
 import shap
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
