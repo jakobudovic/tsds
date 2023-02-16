40cc57d93cad3732a5e76b18a2f52c8d0adb5508
jakobudovic <ju8168@student.uni-lj.si>
2023-02-16 23:02:49 +0100

    Add cross-validation

--- tsds/predict.py
+++ tsds/predict.py
@@ -3,7 +3,7 @@ import shap
 import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.neural_network import MLPClassifier
 
 # shap.initjs()
@@ -20,31 +20,20 @@ def get_predicted_df(
 
     X, y = (
         df_clustering.drop([ids_col], axis=1).copy(),
-        df_bl_visits_clusters[[f"no_cluster_{optimal_cluster_no}"]].copy(),
+        np.ravel(df_bl_visits_clusters[[f"no_cluster_{optimal_cluster_no}"]].copy()),
     )
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, np.ravel(y), test_size=0.3, random_state=22
-    )
-
-    model, explainer = get_best_performing_model(
-        X_train, X_test, y_train, y_test, nn_max_iter
-    )
+    model, explainer = get_best_performing_model(X, y, nn_max_iter)
     print(f"Best performing model for prediction: {model}")
 
-    y_pred = model.predict(df_prediction.drop([ids_col], axis=1).values)
+    model.fit(X, y)
+    y_pred = model.predict(df_prediction.drop([ids_col], axis=1))
     df_prediction.loc[:, "cluster"] = np.ravel(y_pred)  # append predictions
     df_clustering.loc[:, "cluster"] = np.ravel(y)  # append predictions
 
-    return df_clustering, df_prediction, X_train, X_test, model, explainer
+    return df_clustering, df_prediction, model, explainer
 
 
-def get_best_performing_model(
-    X_train,
-    X_test,
-    y_train,
-    y_test,
-    nn_max_iter=150,
-):
+def get_best_performing_model(X, y, nn_max_iter=150):
 
     # 1. Neural network
     nn_clf = MLPClassifier(
@@ -54,31 +43,18 @@ def get_best_performing_model(
         random_state=1,
         max_iter=nn_max_iter,
     )
-    nn_clf.fit(
-        X_train.values, y_train
-    )  # Use X_train.values for SHAP compatibility (avoids warning)
-    y_test_pred = nn_clf.predict(X_test.values)
-    accuracy_nn = 100 * accuracy_score(y_test, y_test_pred)
-
-    print(f"Accuracy on test set for Neural network: {accuracy_nn:.3f}%")
+    accuracy_nn = np.mean(cross_val_score(nn_clf, X, y, cv=10))
+    print(f"Accuracy on test set for Neural network: {accuracy_nn * 100:.2f}%")
 
     # 2. SVC
     svc_clf = sklearn.svm.SVC(kernel="linear", probability=True)
-    svc_clf.fit(X_train.values, y_train)
-    y_test_pred = svc_clf.predict(X_test.values)
-
-    accuracy_svc = 100 * accuracy_score(y_test, y_test_pred)
-    print(f"Accuracy on test set for SVC: {accuracy_svc:.3f}%")
+    accuracy_svc = np.mean(cross_val_score(svc_clf, X, y, cv=10))
+    print(f"Accuracy on test set for SVC: {accuracy_svc * 100:.2f}%")
 
     # 3. Random forest
     rf_clf = RandomForestClassifier(n_estimators=20, random_state=42)
-    rf_clf.fit(X_train.values, y_train)
-    y_test_pred = rf_clf.predict(X_test.values)
-
-    accuracy_rf = 100 * accuracy_score(y_test, y_test_pred)
-    print(
-        f"Accuracy on test set for Random forest: {100 * accuracy_score(y_test, y_test_pred):.3f}%"
-    )
+    accuracy_rf = np.mean(cross_val_score(rf_clf, X, y, cv=10))
+    print(f"Accuracy on test set for Random forest: {accuracy_rf * 100:.2f}%")
 
     models_with_acc = {
         accuracy_nn: (nn_clf, shap.KernelExplainer),
